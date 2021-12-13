8ad81a67b6936353f72d8f39c9a909af08259991
Talha-Altair <cooltalha27@gmail.com>
2021-12-13 21:42:44 +0530

    Updates for 2021-12-13 21:42:44

--- providers.tf
+++ providers.tf
@@ -8,3 +8,15 @@ provider "google" {
 
 data "google_client_config" "default" {}
 
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
