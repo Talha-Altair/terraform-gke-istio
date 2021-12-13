1948aead54a5157b22283da7be7b6792ef04ad35
Talha-Altair <cooltalha27@gmail.com>
2021-12-13 22:29:24 +0530

    Updates for 2021-12-13 22:29:24

--- modules/gke/output.tf
+++ modules/gke/output.tf
@@ -1,3 +1,11 @@
 output "status" {
   value = "${var.cluster_name} created"
 }
+
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+output "ca_cert" {
+  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}
--- providers.tf
+++ providers.tf
@@ -10,13 +10,13 @@ data "google_client_config" "default" {}
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${google_container_cluster.primary.endpoint}"
+    host                   = "https://${module.gke-cluster.cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    cluster_ca_certificate = base64decode(module.gke-cluster.ca_cert)
   }
 }
 provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
+  host                   = "https://${module.gke-cluster.cluster_endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(module.gke-cluster.ca_cert)
 }
